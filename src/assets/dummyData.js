3873d3f34c2a88d90bd3a532b6a762a557599cb1
Samir70 <sbetmouni@talktalk.net>
2023-11-22 12:09:32 +0000

    improve dummy data

--- src/assets/dummyData.js
+++ src/assets/dummyData.js
@@ -1,12 +1,24 @@
-export const dummySong = {
+const dummySong1 = {
   id: 0,
-  title: "La la la",
-  artist: "The Laaas",
+  title: "Knocking On Heaven's Door",
+  artist: "Bob Dylan",
+  status: "Gigable",
+}
+const dummySong2 = {
+  id: 1,
+  title: "Bad Moon Rising",
+  artist: "CCR",
+  status: "Gigable",
+}
+const dummySong3 = {
+  id: 2,
+  title: "Proud Mary",
+  artist: "CCR",
   status: "Gigable",
 }
 
 export const dummySongList = [
-  dummySong,
-  dummySong,
-  dummySong,
+  dummySong1,
+  dummySong2,
+  dummySong3,
 ]
